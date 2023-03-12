88f599f39f766877212b6a7d91ef58c45cb62808
Abhimanyu Agarwal <agarwalabhimanyu0709@gmail.com>
2023-03-12 19:58:40 +0530

    initial commit

---
+++ MinSubarray.java
@@ -0,0 +1,28 @@
+import java.nio.charset.MalformedInputException;
+
+public class MinSubarray {
+    public int solve(int A) {
+        int i;
+        int factors=0;
+        for (i=1;i<=Math.sqrt(A);i++)
+        {
+            if(A%i==0)
+            {
+                if(i==A/i){
+                    factors+=1;
+                }
+                else{
+                    factors+=2;
+                }
+            }
+        }
+        return factors;
+    }
+
+    public static void main(String[] args) {
+        MinSubarray cf=new MinSubarray();
+        int result= cf.solve(25);
+        System.out.println(result);
+    }
+}
+}
